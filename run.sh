fc7ef960ac1caee2a10714ec57606489d94db9ac
sibi <85477603+t-sibiraj@users.noreply.github.com>
2025-04-23 01:42:39 +0800

    changed file structure

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 # Compile Java files
 clear
-javac -d out MainApp.java */*.java */*/*.java
+javac -d out $(find src -name "*.java")
 
 # If compilation succeeds, run the app
 #chmod +x run.sh
 #./run.sh
 if [ $? -eq 0 ]; then
     cd out
-    java MainApp
+    java -cp out MainApp
 else
     echo "Compilation failed. Fix the errors before running."
 fi
