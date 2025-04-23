a0b5d428a6eb86657eb74d6948718213df20226a
sibi <85477603+t-sibiraj@users.noreply.github.com>
2025-04-23 20:43:12 +0800

    fixed compilation error

--- src/model/BTOApplication.java
+++ src/model/BTOApplication.java
@@ -88,7 +88,7 @@ public class BTOApplication {
      */
     public void withdraw() {
         this.wantWithdraw = true;
-        this.status = ApplicationStatus.WITHDRAWN
+        this.status = ApplicationStatus.WITHDRAWN;
         System.out.println("Withdrawal request submitted successfully.");
     }
 
